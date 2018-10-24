b912a7223c73c769a95cc7c7b8a1ab2d21e876f8
Danilo Kallan <danilokallan@hotmail.com>
2018-10-24 16:41:21 +0200

    Created a basic project with the required dependencies and created an SDL window.

---
+++ Magma/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+#include "SDL.h"
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+static bool g_Running = true;
+
+static void HandleKeyEvent(const SDL_Event &event)
+{
+	switch (event.key.keysym.sym)
+	{
+	case SDLK_ESCAPE:
+		g_Running = false;
+		break;
+
+	default:
+		break;
+	}
+}
+
+static void HandleEvent(const SDL_Event &event)
+{
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		g_Running = false;
+		break;
+
+	case SDL_KEYDOWN:
+		HandleKeyEvent(event);
+		break;
+
+	default:
+		break;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Window* window;
+	SDL_Surface* surface;
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// Create an SDL window.
+	window = SDL_CreateWindow("Magma Renderer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+	if (!window)
+	{
+		std::cout << "Unable to create a SDL window." << std::endl;
+		return -1;
+	}
+
+	// Get the window's surface.
+	surface = SDL_GetWindowSurface(window);
+	if (!surface)
+	{
+		std::cout << "Unable to get the window's surface." << std::endl;
+		return false;
+	}
+
+	// Clear the surface to black.
+	SDL_FillRect(surface, 0, 0);
+
+	// Copy the surface to the window.
+	SDL_UpdateWindowSurface(window);
+
+	while (g_Running)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+			HandleEvent(event);
+
+		// Lock the surface.
+		SDL_LockSurface(surface);
+
+		// Draw stuff.
+
+	
+		// Unlock and update the surface.
+		SDL_UnlockSurface(surface);
+		SDL_UpdateWindowSurface(window);
+	}
+
+	// Let SDL clean up its stuff.
+	SDL_Quit();
+
+	return 0;
+}
